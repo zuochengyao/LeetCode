0b7a24f942961515647415f80b20ddd3eb1bdf07
Cheero <zuochengyao@megvii.com>
2022-09-14 16:36:31 +0800

    最长公共前缀

--- src/com/icheero/Program.java
+++ src/com/icheero/Program.java
@@ -1,6 +1,5 @@
 package com.icheero;
 
-
 import com.icheero.dp.Knapsack01;
 import com.icheero.dp.MinDistance;
 import com.icheero.dp.MinPathSum;
@@ -21,115 +20,102 @@ import com.icheero.math.ReverseInteger;
 import com.icheero.math.RomanToInteger;
 import com.icheero.string.AddBinary;
 import com.icheero.string.LengthOfLongestSubstring;
+import com.icheero.string.LongestCommonPrefix;
 import com.icheero.string.UncommonFromSentences;
 import com.icheero.sort.SortUtil;
 
 import java.util.Arrays;
 
-
 @SuppressWarnings("unused")
-public class Program
-{
-    public static void main(String[] args)
-    {
-        knapsack01();
+public class Program {
+    public static void main(String[] args) {
+        longestCommonPrefix();
     }
 
     // region Array
-    private static void plusOne()
-    {
-        int[] array = PlusOne.solution(new int[]{9, 9, 9, 9});
+    private static void plusOne() {
+        int[] array = PlusOne.solution(new int[] {9, 9, 9, 9});
         System.out.println(Arrays.toString(array));
     }
 
-    private static void findDiagonalOrder()
-    {
-        int[] array = FindDiagonalOrder.solution(new int[][]{{1, 2}, {3, 4}, {5, 6}});
+    private static void findDiagonalOrder() {
+        int[] array = FindDiagonalOrder.solution(new int[][] {{1, 2}, {3, 4}, {5, 6}});
         System.out.println(Arrays.toString(array));
     }
 
-    private static void dominantIndex()
-    {
-        System.out.println(DominantIndex.solution(new int[]{3, 6, 1, 0}));
+    private static void dominantIndex() {
+        System.out.println(DominantIndex.solution(new int[] {3, 6, 1, 0}));
     }
 
-    private static void pivotIndex()
-    {
-        System.out.println(PivotIndex.solutionByViolent(new int[]{1, 7, 3, 6, 5, 6}));
-        System.out.println(PivotIndex.solutionBySum(new int[]{1, 7, 3, 6, 5, 6}));
+    private static void pivotIndex() {
+        System.out.println(PivotIndex.solutionByViolent(new int[] {1, 7, 3, 6, 5, 6}));
+        System.out.println(PivotIndex.solutionBySum(new int[] {1, 7, 3, 6, 5, 6}));
     }
 
-    private static void binarySearch()
-    {
-        System.out.println(BinarySearch.solution(new int[]{-1, 0, 3, 5, 9, 12}, 9));
+    private static void binarySearch() {
+        System.out.println(BinarySearch.solution(new int[] {-1, 0, 3, 5, 9, 12}, 9));
     }
 
-    private static void majorityElement()
-    {
+    private static void majorityElement() {
         int[] array = {7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 7, 7, 7, 7};
         System.out.println(MajorityElement.byMooreVote(array));
     }
 
-    private static void kidsWithCandies()
-    {
-        System.out.println(KidsWithCandies.solution(new int[]{2, 3, 5, 1, 3}, 3));
+    private static void kidsWithCandies() {
+        System.out.println(KidsWithCandies.solution(new int[] {2, 3, 5, 1, 3}, 3));
     }
 
-    private static void moveZeroes()
-    {
-        MoveZeroes.solution(new int[]{1, 0, 2, 3, 0, 12});
+    private static void moveZeroes() {
+        MoveZeroes.solution(new int[] {1, 0, 2, 3, 0, 12});
     }
     // endregion
 
     // region Linked
-    private static void addTwoNumbers()
-    {
+    private static void addTwoNumbers() {
         System.out.println(AddTwoNumbers.solution());
     }
 
-    private static void mergeTwoLists()
-    {
+    private static void mergeTwoLists() {
         System.out.println(MergeTwoLists.solution());
     }
 
-    private static void hasCycle()
-    {
+    private static void hasCycle() {
         System.out.println(HasCycle.solution());
     }
     // endregion
 
     // region String
-    private static void addBinary()
-    {
+    private static void addBinary() {
         System.out.println(AddBinary.solution("1010", "1011"));
     }
 
-    private static void lengthOfLongestSubstring()
-    {
+    private static void lengthOfLongestSubstring() {
         System.out.println(LengthOfLongestSubstring.slidingWindow("abcabcbb"));
     }
 
-    private static void uncommonFromSentences()
-    {
-        System.out.println(Arrays.toString(UncommonFromSentences.solution("this apple is sweet", "this apple is sour")));
+    private static void uncommonFromSentences() {
+        System.out.println(Arrays.toString(UncommonFromSentences.solution("this apple is sweet",
+            "this apple is sour"
+        )));
+    }
 
+    private static void longestCommonPrefix() {
+        String prefix = LongestCommonPrefix.solution(new String[] {"cir", "car"});
+        System.out.println(prefix);
     }
     // endregion
 
     // region Math
 
-    private static void reverseInteger()
-    {
+    private static void reverseInteger() {
         System.out.println(ReverseInteger.solution(-1234));
     }
 
-    private static void romanToInteger()
-    {
+    private static void romanToInteger() {
         System.out.println(RomanToInteger.solution("MCMXCIV"));
     }
 
-    private static void multiply()
-    {
+    private static void multiply() {
         System.out.println(Multiply.solution("123", "456"));
     }
     // endregion
@@ -137,50 +123,42 @@ public class Program
     // region Sort
     private static int[] target = {49, 38, 65, 97, 23, 22, 76, 1, 5, 8, 2, 0, -1, 22};
 
-    private static void selection()
-    {
+    private static void selection() {
         SortUtil.selectStraight(target);
         System.out.println(Arrays.toString(target));
     }
 
-    private static void insert()
-    {
+    private static void insert() {
         SortUtil.insertStraight(target);
         System.out.println(Arrays.toString(target));
     }
 
-    private static void bubble()
-    {
+    private static void bubble() {
         SortUtil.bubble(target);
         System.out.println(Arrays.toString(target));
     }
 
-    private static void quick()
-    {
+    private static void quick() {
         SortUtil.quick(target);
         System.out.println(Arrays.toString(target));
     }
     // endregion
 
     // region 动态规划 DP
-    private static void uniquePaths()
-    {
+    private static void uniquePaths() {
         System.out.println(UniquePaths.solution(7, 3));
     }
 
-    private static void minPathSum()
-    {
+    private static void minPathSum() {
         int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
         System.out.println(MinPathSum.solution(grid));
     }
 
-    private static void minDistance()
-    {
+    private static void minDistance() {
         System.out.println(MinDistance.solution("horse", "ros"));
     }
 
-    private static void knapsack01()
-    {
+    private static void knapsack01() {
         int[] weights = {2, 2, 6, 5, 4};
         int[] values = {6, 3, 5, 4, 6};
         int v = 10;
---
+++ src/com/icheero/string/LongestCommonPrefix.java
@@ -0,0 +1,52 @@
+package com.icheero.string;
+
+/**
+ * 14. 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 提示：
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] 仅由小写英文字母组成
+ *
+ * https://leetcode.cn/problems/longest-common-prefix/
+ *
+ * @author Work
+ * @date 2022-09-14
+ */
+public class LongestCommonPrefix {
+    public static String solution(String[] strs) {
+        String ret = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() == 0)
+                return "";
+            char[] chars = strs[i].toCharArray();
+            int index = 0;
+
+            int len = Math.min(ret.length(), chars.length);
+
+            for (int j = 0; j < len; j++) {
+                if (chars[j] == ret.charAt(j)) {
+                    index++;
+                } else {
+                    break;
+                }
+            }
+            ret = ret.substring(0, index);
+        }
+        return ret;
+    }
+
+    public static String solutionBetter(String[] strs) {
+        //公共前缀比所有字符串都短，随便选一个先
+        String s = strs[0];
+        for (String string : strs) {
+            while (!string.startsWith(s)) {
+                //公共前缀不匹配就让它变短！
+                s = s.substring(0, s.length() - 1);
+            }
+        }
+        return s;
+    }
+}
